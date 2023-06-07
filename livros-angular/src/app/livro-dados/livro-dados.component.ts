fcbb21f11fe951fa12fb0396741ab7394a642538
AlexandreJauer <alexandrejauer@hotmail.com>
2023-06-06 22:26:11 -0300

    atualização 01

--- livros-angular/src/app/livro-dados/livro-dados.component.ts
+++ livros-angular/src/app/livro-dados/livro-dados.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ControleEditoraService } from '../controle-editora.service';
+import { ControleLivrosService } from '../controle-livros.service';
+import { Livro } from '../livro';
+import { Editora } from '../editora'; 
+
 
 @Component({
   selector: 'app-livro-dados',
   templateUrl: './livro-dados.component.html',
-  styleUrls: ['./livro-dados.component.scss']
+  styleUrls: ['./livro-dados.component.css']
 })
-export class LivroDadosComponent {
+export class LivroDadosComponent implements OnInit {
+  livro: Livro = new Livro(0, 0, '', '', []);
+  autoresForm: string = '';
+  editoras: Array<Editora> = [];
 
+  constructor(
+    private servEditora: ControleEditoraService,
+    private servLivros: ControleLivrosService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.editoras = this.servEditora.getEditoras();
+  }
+
+  incluir = (): void => {
+    this.livro.autores = this.autoresForm.split('\n');
+    this.servLivros.incluir(this.livro);
+    this.router.navigateByUrl('/lista');
+  }
 }
